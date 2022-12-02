26dfe6513df797939bef4b4576459b7024d081d0
Abdullah Saleem <abdullahsaleem40404@gmail.com>
2022-12-02 12:04:51 +0000

    API Work Done with checkCredit ratelimit auth logging function on two api get...

---
+++ app/backend/api_gateway/routes.js
@@ -0,0 +1,32 @@
+const ROUTES = [
+    {
+        url: '/free',
+        auth: false,
+        creditCheck: true,
+        rateLimit: {
+            windowMs: 0.5*60*1000,
+            max: 2
+        },
+        proxy: {
+            target: "https://www.google.com",
+            changeOrigin: true,
+            pathRewrite: {
+                [`^/free`]: '',
+            },
+        }
+    },
+    {
+        url: '/premium',
+        auth: true,
+        creditCheck: true,
+        proxy: {
+            target: "https://www.google.com",
+            changeOrigin: true,
+            pathRewrite: {
+                [`^/premium`]: '',
+            },
+        }
+    }
+]
+
+exports.ROUTES = ROUTES;
