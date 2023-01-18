0a940ea192d0b6174332000af0f19b16e7dce683
abdullah saleem <abdullahsaleem40404@gmail.com>
2023-01-18 21:20:57 +0500

    graphql

--- app/backend/api_gateway/server.js
+++ app/backend/api_gateway/server.js
@@ -1,27 +1,3 @@
-// const express = require('express')
-
-// const {ROUTES} = require("./routes");
-
-// const {setupLogging} = require("./logging");
-// const {setupRateLimit} = require("./ratelimit");
-// const {setupCreditCheck} = require("./checkCredit");
-// const {setupProxies} = require("./proxy");
-// const {setupAuth} = require("./auth");
-
-// const app = express()
-// const port = 4000;
-
-
-// setupLogging(app);
-// setupRateLimit(app, ROUTES);
-// setupAuth(app, ROUTES);
-// setupCreditCheck(app, ROUTES);
-// setupProxies(app, ROUTES);
-
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`)
-// })
-
 const port = 3000;
 
 const express = require('express')
