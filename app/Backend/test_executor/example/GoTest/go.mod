306d5ec9bf6c2f869957f2c9096c69eaea2e1a11
Abdullah-40404 <abdullahsaleem40404@gmail.comgit config --global user.name>
2023-03-14 15:15:32 +0500

    Settings Final

--- app/Backend/test_executor/example/GoTest/go.mod
+++
@@ -1,7 +0,0 @@
-module Module1
-
-go 1.19
-
-require github.com/tebeka/selenium v0.9.9
-
-require github.com/blang/semver v3.5.1+incompatible // indirect
--- app/Backend/test_executor/graph/model/test.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Test struct {
-	ID       int    `json:"id"`
-	Status   string `json:"status"`
-	TestPath string `json:"testPath"`
-}
