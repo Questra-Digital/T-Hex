1554570f2a48d4560b897f976693d82c2abbf482
Nafees Hassan <nafeeshassan16@gmail.com>
2024-11-01 11:30:33 +0500

    Implemented server side proxy

---
+++ hub/proxse/go.mod
@@ -0,0 +1,3 @@
+module proxse
+
+go 1.23.2
--- hub/proxse/main.go
+++ hub/proxse/main.go
@@ -5,29 +5,43 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 )
 
 func main() {
-	// selenium grid URL
-	targetURL, _ := url.Parse("http://localhost:4444")
+	endpoint := os.Getenv("THEX_URL")
+	local := os.Getenv("LOCAL")
+	if endpoint == "" {
+		endpoint = "http://localhost:4444"
+	}
+	if local == "" {
+		local = ":4445"
+	}
+	targetURL, err := url.Parse(endpoint)
+	if err != nil {
+		log.Printf("Error parsing THEX_URL `%s`:\n\t%s", endpoint, err.Error())
+		os.Exit(1)
+	}
 
-	// Set up the reverse proxy
 	proxy := httputil.NewSingleHostReverseProxy(targetURL)
 	proxy.Director = func(req *http.Request) {
-		log.Printf("Intercepted request: %s %s", req.Method, req.URL.Path)
-		log.Printf("Headers: %v", req.Header)
 
-		// TODO: remove this
-		if req.Method == "DELETE" {
-			return
-		}
-
-		// forward
 		req.URL.Scheme = targetURL.Scheme
 		req.URL.Host = targetURL.Host
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// TODO: log to DB
+		key := r.Header.Get("thex-key")
+		proj := r.Header.Get("thex-proj")
+		log.Printf("key: `%s`, proj: `%s`", key, proj)
+
+		if !KeyIsValid(key) {
+			log.Print("Dropped due to Unauthorized")
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
 		proxy.ServeHTTP(w, r)
 	})
 
@@ -35,4 +49,12 @@ func main() {
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
+}
+
+func KeyIsValid(key string) bool {
+	if key == "" {
+		return false
+	}
+	// TODO check in DB
+	return true
 }
