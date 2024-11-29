da1abe1953d95a2c08d6853fe28c83f296212405
Nafees Hassan <nafeeshassan16@gmail.com>
2024-11-29 06:25:15 +0500

    Better logging in ProxCE

--- hub/proxce/main.go
+++ hub/proxce/main.go
@@ -13,19 +13,23 @@ func main() {
 	local := os.Getenv("LOCAL")
 	key := os.Getenv("THEX_KEY")
 	proj := os.Getenv("THEX_PROJ")
-	if !KeyIsValid(key) {
-		log.Fatalf("Cannot run with invalid key")
+	if key == "" {
+		log.Fatalf("THEX_KEY env var is not set.")
 	}
 	if endpoint == "" {
 		endpoint = "http://localhost:4445"
+		log.Printf("Using default `%s` endpoint", endpoint)
+		log.Println("\tConsider setting the THEX_URL env var")
 	}
 	if local == "" {
 		local = ":4444"
+		log.Printf("Using default `%s` for listener", local)
+		log.Println("\tConsider setting the LOCAL env var")
 	}
 	if proj == "" {
 		proj = "UNNAMED PROJECT"
-		log.Println("Using default `UNNAMED PROJECT` for name.")
-		log.Println("Consider setting the THEX_PROJ env var")
+		log.Printf("Using default `%s` for name.", proj)
+		log.Println("\tConsider setting the THEX_PROJ env var")
 	}
 	targetURL, err := url.Parse(endpoint)
 	if err != nil {
@@ -41,10 +45,7 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf(
-			"Request received: %s %s\n\tkey: `%s`\n\tproj: `%s`\n\tHeaders: %v\n",
-			r.Method, r.URL.Path, key, proj, r.Header,
-		)
+		log.Printf("%s %s", r.Method, r.URL.String())
 		proxy.ServeHTTP(w, r)
 	})
 
@@ -53,12 +54,4 @@ func main() {
 	if err := http.ListenAndServe(local, nil); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
-}
-
-func KeyIsValid(key string) bool {
-	if key == "" {
-		return false
-	}
-	// TODO do the check
-	return true
 }
