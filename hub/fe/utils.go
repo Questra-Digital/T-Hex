687cc18b0fac478e40635de8475ca4a9d0ecdec3
Nafees Hassan <nafeeshassan16@gmail.com>
2024-12-21 06:54:42 +0500

    Validation functions

--- hub/fe/utils.go
+++ hub/fe/utils.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"net/mail"
+	"unicode"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -15,4 +18,26 @@ func PasswordHash(password string) (string, error) {
 func PasswordHashMatch(hash, plain string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(plain))
 	return err == nil
+}
+
+// whether a string is a valid email address
+func EmailIsValid(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err != nil
+}
+
+func UsernameIsValid(username string) bool {
+	if len(username) < 4 {
+		return false
+	}
+	for i, r := range username {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			continue
+		}
+		if r == '_' && i > 0 && i < len(username)-1 {
+			continue
+		}
+		return false
+	}
+	return true
 }
