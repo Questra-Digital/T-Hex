9a833cb72776d2181d68280e537698168625c1d6
Nafees Hassan <nafeeshassan16@gmail.com>
2024-12-24 07:10:14 +0500

    Added SingleUseToken stuff

--- hub/fe/utils.go
+++ hub/fe/utils.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"net/mail"
+	"sync"
+	"time"
 	"unicode"
 
 	"golang.org/x/crypto/bcrypt"
@@ -63,4 +67,58 @@ func UserGetCurrentKey(username string) string {
 		return ""
 	}
 	return result.Key
+}
+
+var SingleUseTokens = struct {
+	sync.RWMutex
+	data map[string]struct{Time time.Time; Key string}
+}{data: make(map[string]struct{Time time.Time; Key string})}
+
+func SingleUseTokenGenerate(username string) string {
+	for {
+		tokenBytes := make([]byte, 64)
+		_, err := rand.Read(tokenBytes)
+		if err != nil {
+			panic("Error generating random token:" + err.Error())
+		}
+		token := hex.EncodeToString(tokenBytes)
+		SingleUseTokens.RLock()
+		if _, exists := SingleUseTokens.data[token]; exists {
+			tokenStoreMu.RUnlock()
+			continue
+		}
+		SingleUseTokens.RUnlock()
+		expiresAt := time.Now().Add(2 * time.Minute)
+		SingleUseTokens.Lock()
+		SingleUseTokens.data[username] = struct {
+			Time time.Time; Key string}{expiresAt, token}
+		SingleUseTokens.Unlock()
+		return token
+	}
+}
+
+func SingleUseTokenGet(username string) string {
+	SingleUseTokens.Lock()
+	defer SingleUseTokens.Unlock()
+	if tokenData, exists := SingleUseTokens.data[username]; exists {
+		if time.Since(tokenData.Time) < 2*time.Minute {
+			return tokenData.Key
+		}
+		delete(SingleUseTokens.data, username)
+	}
+	return ""
+}
+
+func SingleUseTokenInvalidationStart(timer time.Duration) {
+	ticker := time.NewTicker(timer)
+	defer ticker.Stop()
+	for range ticker.C {
+		for username, data := range SingleUseTokens.data {
+			if time.Now().After(data.Time) {
+				SingleUseTokens.Lock()
+				delete(SingleUseTokens.data, username)
+				SingleUseTokens.Unlock()
+			}
+		}
+	}
 }
