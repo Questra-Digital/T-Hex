256a5df4c3e9100cadca2b6290b52c3212a075c7
bilal-os <mb561366@gmail.com>
2025-02-14 17:31:18 +0500

    Implemented Authentication Midlleware based upon JWT Tokens

--- hub/web_app/server/main.go
+++ hub/web_app/server/main.go
@@ -28,8 +28,7 @@ type Credentials struct{
 	Password string `json:"password"`
 }
 
-func loginHandler(w http.ResponseWriter,r *http.Request)
-{
+func loginHandler(w http.ResponseWriter,r *http.Request){
 	//Only POST Methods are allowed
 	if(r.Method != http.MethodPost)
 	{
@@ -74,4 +73,64 @@ func loginHandler(w http.ResponseWriter,r *http.Request)
 	w.Header().Set("Content-Type","application/json")
 	json.NewEncoder(w).Encode(map[string]string{"token":tokenString})
 
+}
+
+func generateJWT(username string) (string,error){
+
+	//Expiration is set to 5 days
+	expirationTime := time.Now().Add(5 * 24 * time.Hour)
+
+	//Pointer to Claims is created, as jwt library expects a pointer
+	claims := &Claims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},	
+	}
+
+	//Create new token with claims as payload
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	//Sign the token with the secret key
+	return token.SignedString(jwtsecret)
+
+}
+
+func authMiddleware(next http.HandlerFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+
+		//Extract token from Authorization Header Field
+		tokenString := r.Header.Get("Authorization")
+
+		if tokenString == ""{
+			http.Error(w,"Missing Token",http.StatusUnauthorized)
+			return
+		}
+
+		//Parse and validate the token
+		claims := &Claims{}
+		token,err := jwt.ParseWithClaims(tokenString,claims,func (token *jwt.Token) (interface{},error){
+			return jwtsecret, nil
+		})
+
+		//Set request header for subsequent handlers
+		r.Header.Set("Username",claims.Username)
+
+		//Call the next handler
+		next(w,r)
+
+	}
+}
+
+func dashboardHandler(w http.ResponseWriter, r *http.Request)
+{
+	username := r.Header.Get("Username")
+	fmt.Fprintf(w,"Welcome to the Dashboard, %s!",username)
+}
+
+func main(){
+	http.HandleFunc("/login",loginHandler)
+	http.HandleFunc("/dashboard",authMiddleware(dashboardHandler))
+	fmt.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
