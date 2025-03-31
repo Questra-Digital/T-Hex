67c2f086c7d581e45e79f91929fdf7a7dde4fe32
bilal-os <mb561366@gmail.com>
2025-03-31 08:53:09 +0500

    Implemented proper json response

--- hub/web_app/server/handlers/dashboard.go
+++ hub/web_app/server/handlers/dashboard.go
@@ -3,9 +3,13 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"server/utils"
 )
 
 func DashboardHandler(w http.ResponseWriter, r *http.Request){
 	username := r.Header.Get("Username")
-	fmt.Fprintf(w,"Welcome to the Dashboard, %s!",username)
+	message := fmt.Sprintf("Welcome to the Dashboard, %s!", username)
+	utils.RespondJSON(w, http.StatusOK, map[string]string{
+		"message": message,
+	})
 }
