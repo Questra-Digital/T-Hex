4019ad246f2ae4767fc9de4df85e9898a2dcbe1e
bilal-os <mb561366@gmail.com>
2025-04-03 19:15:47 +0500

    Constructed a map for state store simulating DB

--- hub/web_app/server/models/models.go
+++ hub/web_app/server/models/models.go
@@ -1,6 +1,7 @@
 package models
 
 import "github.com/golang-jwt/jwt/v4"
+import "sync"
 
 type Claims struct{
 	Username string `json:"username"`
@@ -16,3 +17,9 @@ type Credentials struct{
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
+
+// In-memory state storage
+var StateStore = struct {
+	sync.RWMutex
+	Map map[string]string
+}{Map: make(map[string]string)}
