8da71e972cba5f1aa55b8cecbf798fddd7468924
bilal-os <mb561366@gmail.com>
2025-04-03 19:14:59 +0500

    Moved loading env file to config directory

--- hub/web_app/server/main.go
+++ hub/web_app/server/main.go
@@ -6,11 +6,17 @@ import (
 	"server/handlers"
 	"server/middleware"
 	"net/http" 
+	"server/config"
 )
 
 func main(){
+
+	config.LoadEnv()
+
 	http.HandleFunc("/login",handlers.LoginHandler)
 	http.HandleFunc("/dashboard",middleware.AuthMiddleware(handlers.DashboardHandler))
+	http.HandleFunc("/googleLogin",handlers.GoogleLogin)
+	http.HandleFunc("/auth/google/callback",handlers.GoogleCallback)
 	fmt.Println("Server is running on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
