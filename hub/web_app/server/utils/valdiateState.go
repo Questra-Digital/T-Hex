b2deab0b3b0125b429b322e897b0ba242ba60be1
bilal-os <mb561366@gmail.com>
2025-04-14 22:40:22 +0500

    Extracted a method to delete a state

--- hub/web_app/server/utils/valdiateState.go
+++ hub/web_app/server/utils/valdiateState.go
@@ -6,15 +6,20 @@ import (
 )
 
 
-// ValidateState checks if the state exists and deletes it to prevent replay attacks
-func ValidateState(state string) bool {
-	models.StateStore.Lock()
-	defer models.StateStore.Unlock()
-
+func DeleteState(state string) bool{
 	_, exists := models.StateStore.Map[state]
 	if exists {
 		delete(models.StateStore.Map, state)
 		fmt.Println("State validated and deleted:", state)
 	}
+	return exists
+}
+
+// ValidateState checks if the state exists and deletes it to prevent replay attacks
+func ValidateState(state string) bool {
+	models.StateStore.Lock()
+	defer models.StateStore.Unlock()
+
+	exists := DeleteState(state)
 	return exists
 }
