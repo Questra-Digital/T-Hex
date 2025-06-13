a5ae935fa7927d8e8bbc3a3680af7b572033887f
bilal-os <mb561366@gmail.com>
2025-06-13 16:35:23 +0500

    feat(header): implemented a sperate landingPage route and a rough Header Component under components directory

---
+++ hub/web_app/frontend/src/app/landingPage/layout.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header";
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
