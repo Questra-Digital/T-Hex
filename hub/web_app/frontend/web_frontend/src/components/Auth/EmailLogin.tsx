44e2cc10fe540d64e68d5b50a42fbe87a630ceb4
bilal-os <mb561366@gmail.com>
2025-06-13 15:59:18 +0500

    refactor(frontend): scraped previous boiler code and scaffolded new one at higher level of directory

--- hub/web_app/frontend/web_frontend/src/components/Auth/EmailLogin.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export const EmailLogin = () => {
-  return (
-    <button className="flex items-center justify-center px-8 py-3.5 bg-[#3D8D7A] text-white rounded-lg shadow-lg hover:bg-[#327264] hover:shadow-xl transition-all w-full sm:w-auto text-base font-medium">
-      Start Free with Email
-    </button>
-  );
-}; 
