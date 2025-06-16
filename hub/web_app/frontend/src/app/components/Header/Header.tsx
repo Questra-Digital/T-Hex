484eda3e746704be6e77678204d7b7515189f254
bilal-os <mb561366@gmail.com>
2025-06-16 22:23:57 +0500

    Implemented Close icon for menu

--- hub/web_app/frontend/src/app/components/Header/Header.tsx
+++ hub/web_app/frontend/src/app/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { montserrat, outfit } from "@/font/fonts";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import MenuDrawer from "./MenuDrawer/MenuDrawer";
 
 const links = [
   { name: "Home", href: "/landingPage" },
@@ -15,55 +16,60 @@ const links = [
 ];
 
 export default function Header() {
-  const [isSmall, setIsSmall] = useState(false);
-  const [isMedium, setIsMedium] = useState(false);
-  const [isLarge, setIsLarge] = useState(false);
-  const [screenSize, setScreenSize] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  function onMenuPressed() {
+    setIsDrawerOpen(!isDrawerOpen);
+  }
 
   return (
-    <header
-      className={`${styles.header} ${montserrat.variable} ${outfit.variable}`}
-    >
-      {/* Menu Icon */}
-      <Image
-        src="/Icons/Menu.svg"
-        alt="Menu Icon"
-        className={styles.menuIcon}
-        width={58}
-        height={58}
-      />
+    <>
+      <header
+        className={`${styles.header} ${montserrat.variable} ${outfit.variable}`}
+      >
+        {/* Menu Icon */}
+        <Image
+          src={isDrawerOpen ? "/Icons/close.svg" : "/Icons/Menu.svg"}
+          alt={isDrawerOpen ? "Close menu" : "Open menu"}
+          className={styles.menuIcon}
+          width={42}
+          height={42}
+          onClick={onMenuPressed}
+        />
 
-      {/* T-Hex Logo */}
-      <h1>T-Hex</h1>
-      
-      {/* Nav Bar */}
-      <nav>
-        <ul>
-          {/*map each link to Next's Link*/}
-          {links.map((link) => {
-            return (
-              <li key={link.name}>
-                <Link href={link.href}>
-                  {link.name}
-                  <Image
-                    height={32}
-                    width={32}
-                    src="/Icons/arrow_down.svg"
-                    alt="Arrow Down Icon"
-                    className={styles.arrowIcon}
-                  />
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+        {/* T-Hex Logo */}
+        <h1>T-Hex</h1>
 
-      {/* Button Groups */}
-      <div className={styles.buttonsGroup}>
-        <button>Login</button>
-        <button className={styles.getStarted}>Get Started Free</button>
-      </div>
-    </header>
+        {/* Nav Bar */}
+        <nav>
+          <ul>
+            {/*map each link to Next's Link*/}
+            {links.map((link) => {
+              return (
+                <li key={link.name}>
+                  <Link href={link.href}>
+                    {link.name}
+                    <Image
+                      height={32}
+                      width={32}
+                      src="/Icons/arrow_down.svg"
+                      alt="Arrow Down Icon"
+                      className={styles.arrowIcon}
+                    />
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
+        {/* Button Groups */}
+        <div className={styles.buttonsGroup}>
+          <button>Login</button>
+          <button className={styles.getStarted}>Get Started Free</button>
+        </div>
+      </header>
+      {isDrawerOpen && <MenuDrawer links={links} />}
+    </>
   );
 }
