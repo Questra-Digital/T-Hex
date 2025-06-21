b6a5562c10c520e40a23bffdde9f123c7d6e8406
bilal-os <mb561366@gmail.com>
2025-06-21 21:53:07 +0500

    added short reference paths for Saas files

--- hub/web_app/frontend/next.config.ts
+++ hub/web_app/frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    includePaths: ['./src/styles'],
+  },
 };
 
 export default nextConfig;
