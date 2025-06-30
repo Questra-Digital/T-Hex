42123264d858095fcad05b1dcd5ad2ec55eaf895
bilal-os <mb561366@gmail.com>
2025-06-30 11:30:30 +0500

    refactor: improve MenuDrawer component structure and extract constants

--- hub/web_app/frontend/src/components/Header/MenuDrawer/MenuDrawer.tsx
+++ hub/web_app/frontend/src/components/Header/MenuDrawer/MenuDrawer.tsx
@@ -1,6 +1,6 @@
 import styles from "@/styles/components/Header/MenuDrawer.module.scss";
 import Image from "next/image";
-import { outfit } from "@/font/fonts";
+import { outfit } from "../../../fonts/fonts";
 import { useEffect, useState } from "react";
 
 interface Link {
@@ -13,7 +13,11 @@ interface MenuDrawerProps {
   isDrawerOpen: boolean;
 }
 
-export default function Drawer({
+// Constants
+const ANIMATION_DURATION = 300; // milliseconds
+const ARROW_ICON_SIZE = 34;
+
+export default function MenuDrawer({
   links,
   isDrawerOpen = false,
 }: MenuDrawerProps) {
@@ -21,44 +25,53 @@ export default function Drawer({
 
   useEffect(() => {
     if (!isDrawerOpen) {
-      const timer = setTimeout(() => setShouldRender(false), 300); //wait for 3 milliseconds so that closing class takes effect
-      return () => clearTimeout(timer); //CLeanup timer
+      const timer = setTimeout(() => setShouldRender(false), ANIMATION_DURATION);
+      return () => clearTimeout(timer);
     } else {
       setShouldRender(true);
     }
   }, [isDrawerOpen]);
 
-  return (
+  const renderNavigationLinks = () => (
     <>
-      {shouldRender && (
-        <div
-          className={`${styles.drawerContainer} ${
-            !isDrawerOpen ? styles.closing : ""
-          }`}
-        >
-          {links.map((link) => (
-            <div key={link.name} className={styles.linkCard}>
-              <p className={`${styles.linkName} ${outfit.variable}`}>
-                {link.name}
-              </p>
-              <Image
-                height={34}
-                width={34}
-                src="/Icons/arrow_down.svg"
-                alt="Arrow Down Icon"
-                className={styles.arrowIcon}
-              />
-            </div>
-          ))}
-
-          <div className={`${styles.linkCard}`}>
-            <button className={outfit.variable}>Login</button>
-            <button className={`${styles.getStarted} ${outfit.variable}`}>
-              Get Started Free
-            </button>
-          </div>
+      {links.map((link) => (
+        <div key={link.name} className={styles.linkCard}>
+          <p className={`${styles.linkName} ${outfit.variable}`}>
+            {link.name}
+          </p>
+          <Image
+            height={ARROW_ICON_SIZE}
+            width={ARROW_ICON_SIZE}
+            src="/Icons/arrow_down.svg"
+            alt="Arrow Down Icon"
+            className={styles.arrowIcon}
+          />
         </div>
-      )}
+      ))}
     </>
+  );
+
+  const renderButtons = () => (
+    <div className={styles.linkCard}>
+      <button className={outfit.variable}>Login</button>
+      <button className={`${styles.getStarted} ${outfit.variable}`}>
+        Get Started Free
+      </button>
+    </div>
+  );
+
+  if (!shouldRender) {
+    return null;
+  }
+
+  return (
+    <div
+      className={`${styles.drawerContainer} ${
+        !isDrawerOpen ? styles.closing : ""
+      }`}
+    >
+      {renderNavigationLinks()}
+      {renderButtons()}
+    </div>
   );
 }
