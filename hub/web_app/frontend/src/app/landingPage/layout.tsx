e6709fc2a1c13175fc386ceb09452bf1e6ac1243
bilal-os <mb561366@gmail.com>
2025-06-30 16:57:02 +0500

    updated layout to use div as a wrapper and included landingPage style

--- hub/web_app/frontend/src/app/landingPage/layout.tsx
+++ hub/web_app/frontend/src/app/landingPage/layout.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/Header/Header";
+import styles from "@/styles/pages/LandingPage.module.scss";
+import { outfit } from "@/fonts/fonts";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
+    <div className={`${styles.landingPage} ${outfit.variable}`}>
+      <header>
         <Header />
-        {children}
-      </body>
-    </html>
+      </header>
+      {children}
+    </div>
   );
 }
