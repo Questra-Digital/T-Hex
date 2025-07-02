e38ac03fd643fdbfe0dbf18c925ec51ed588dc62
bilal-os <mb561366@gmail.com>
2025-07-02 16:39:49 +0500

    Added WhatItDo section and re-ordered the sections

--- hub/web_app/frontend/src/app/landingPage/page.tsx
+++ hub/web_app/frontend/src/app/landingPage/page.tsx
@@ -2,13 +2,15 @@ import HeroSection from "@/components/HeroSection";
 import FeaturesSection from "@/components/FeaturesSection";
 import PricingSection from "@/components/PricingSection";
 import IntegrationsSection from "@/components/IntegrationsSection";
+import WhatItDo from "@/components/WhatItDoSection";
 export default function Page() {
   return (
     <>
       <HeroSection />
+      <WhatItDo />
       <FeaturesSection />
-      <PricingSection />
       <IntegrationsSection />
+      <PricingSection />
     </>
   );
 }
