acc1d5f59d1bf3ccefdf5c0fa153e6d6b9b48eef
bilal-os <mb561366@gmail.com>
2025-10-11 22:02:17 +0500

    added optional status code field

--- hub/web_app/frontend/src/types/API.ts
+++ hub/web_app/frontend/src/types/API.ts
@@ -6,10 +6,14 @@ export type JSONValue =
   | JSONValue[]
   | { [key: string]: JSONValue };
 
-
+/*
+  APIResponse is a generic response type for all API responses.
+  It contains a success flag, a message, an error (if any), data (if any), and a status code (if any).
+*/
 export interface APIResponse<T = JSONValue> {
   success: boolean;
   message: string;
   error?: string;
   data?: T;
+  statusCode?: number;
 }
